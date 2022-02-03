4556070b0109be6cffc276d016825e65161a7f4f
metalabyss <colorful.hoshi@gmail.com>
2022-02-03 16:14:38 +0700

    Put menu items into favorites fragment's toolbar.

--- app/src/main/java/io/farafonova/weatherapp/WeatherFavoritesFragment.kt
+++ app/src/main/java/io/farafonova/weatherapp/WeatherFavoritesFragment.kt
@@ -1,15 +1,19 @@
 package io.farafonova.weatherapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import io.farafonova.weatherapp.databinding.FragmentWeatherFavoritesBinding
 
 
 class WeatherFavoritesFragment : Fragment() {
     private var binding: FragmentWeatherFavoritesBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,11 +21,17 @@ class WeatherFavoritesFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentWeatherFavoritesBinding.inflate(layoutInflater, container, false)
+        (activity as AppCompatActivity).setSupportActionBar(binding!!.appBar)
         return binding!!.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
